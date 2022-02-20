049a90abd45a4ebb507625157081adcfff5b7a37
Brian Retamar <leo1632@hotmail.com>
2022-02-20 20:49:53 -0300

    removed dragula. Added npm-run-all dependency

--- public/position.js
+++ public/position.js
@@ -1,14 +1,14 @@
 function myFunction() {
 
     var body = document.body
-    console.log(body)
+    // console.log(body)
 
     var refs = document.querySelectorAll(".is-hoverable");
 
     var screenSize = getScreenSize();
 
-    console.log(screenSize)
-    console.log(refs)
+    // console.log(screenSize)
+    // console.log(refs)
 
 
     refs.forEach(el => {
@@ -16,13 +16,13 @@ function myFunction() {
             var mousePosY = evt.clientY
             if (mousePosY < (screenSize.height / 2)) {
                 // estas en la parte superior de la mitad de la pantalla en el eje Y
-                console.log(`estas en la parte INFERIOR. HEIGHT:${screenSize.height} MOUSE_POS_Y:${mousePosY}`)
+                // console.log(`estas en la parte INFERIOR. HEIGHT:${screenSize.height} MOUSE_POS_Y:${mousePosY}`)
                 el.classList.remove('is-up')
-                console.log(el)
+                // console.log(el)
             }
             else {
                 // estas en la parte inferior de la mitad de la pantalla en el eje Y
-                console.log(`estas en la parte INFERIOR. HEIGHT:${screenSize.height} MOUSE_POS_Y:${mousePosY}`)
+                // console.log(`estas en la parte INFERIOR. HEIGHT:${screenSize.height} MOUSE_POS_Y:${mousePosY}`)
                 el.classList.add('is-up')
 
             }
